2ea8bdc4bce15b8c5bcd54018fa848f99e6cda3b
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-19 14:15:51 -0300

    Used the method train_test_split

--- classification/buying_trends.py
+++ classification/buying_trends.py
@@ -1,5 +1,7 @@
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
 import pandas
 from pathlib import Path
 
@@ -11,9 +13,11 @@ data = pandas.read_csv(Path(__file__).parent / "data/ecom.csv")
 features = data[["home", "how_it_works", "contact"]]
 results = data["bought"]
 
+train_features, test_features, train_results, test_results = train_test_split(features, results, random_state = 20, test_size = 0.25)
+
 # Training 75% of the data
 model = LinearSVC()
-model.fit(features[:75], results[:75])
+model.fit(train_features, train_results)
 
 # Test with the remaining 25%
-print("Hit rate: %.2f" % (accuracy_score(results[75:], model.predict(features[75:])) * 100))
+print("Hit rate: %.2f" % (accuracy_score(test_results, model.predict(test_features)) * 100))
