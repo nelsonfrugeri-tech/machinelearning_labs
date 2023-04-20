f5306d61fdd376a69dc99b0dc032ca74d81a83c7
Nelson Frugeri Jr <nfrugeri@gmail.com>
2023-04-20 15:10:46 -0300

    Implementation baseline algorithm and refactor Machine class

--- classification/buying_trends.py
+++ classification/buying_trends.py
@@ -1,4 +1,3 @@
-from sklearn.model_selection import train_test_split
 from pathlib import Path
 from kit_learn import Machine
 
@@ -9,20 +8,4 @@ import pandas
 mac = Machine()
 data = pandas.read_csv(Path(__file__).parent / "data/ecom.csv")
 
-features = data[["home", "how_it_works", "contact"]]
-results = data["bought"]
-
-train_features, test_features, train_results, test_results = train_test_split(features, results, 
-                                                                              random_state = 20, test_size = 0.25,
-                                                                              stratify = results)
-
-# Training 75% of the data
-mac.fit(train_features, train_results)
-
-# Test with the remaining 25%
-mac.hit_rate_in_percent(test_results, mac.predict(test_features))
-
-# Proportion between train and test
-print("Proportion between train and test is: \n")
-print(train_results.value_counts(), "\n")
-print(test_results.value_counts(), "\n")
+mac.classification(data[["home", "how_it_works", "contact"]], data["bought"], 20, 0.25)
--- classification/work_finished.py
+++ classification/work_finished.py
@@ -1,34 +1,27 @@
 from pathlib import Path
-from sklearn.model_selection import train_test_split
 from kit_learn import Machine
 
 import seaborn
 import pandas
 import matplotlib.pyplot as plt
+    
+def dashboards(data):
+    seaborn.scatterplot(x = "expected_hours", y = "price", hue = "finished", data=data)
+    seaborn.relplot(x = "expected_hours", y = "price", hue = "finished", col = "finished", data=data)
 
-mac = Machine()
+    plt.show()
 
-data = pandas.read_csv(Path(__file__).parent / "data/work_finished.csv")
-data["finished"] = data.unfinished.map({
-    0: 1,
-    1: 0
-})
+def main():
+    mac = Machine()
+    data = pandas.read_csv(Path(__file__).parent / "data/work_finished.csv")
+    data["finished"] = data.unfinished.map({
+        0: 1,
+        1: 0
+    })
 
-features = data[["expected_hours", "price"]]
-results = data["finished"]
+    mac.baseline(data["finished"], 0.25)
+    mac.classification(data[["expected_hours", "price"]], data["finished"], 20, 0.25)
+    # dashboards(data)
 
-train_features, test_features, train_results, test_results = train_test_split(features, results, 
-                                                                              random_state = 20, test_size = 0.25,
-                                                                              stratify = results)
-
-# Training 75% of the data
-mac.fit(train_features, train_results)
-
-# Test with the remaining 25%
-mac.hit_rate_in_percent(test_results, mac.predict(test_features))
-
-seaborn.scatterplot(x = "expected_hours", y = "price", hue = "finished", data=data)
-seaborn.relplot(x = "expected_hours", y = "price", hue = "finished", col = "finished", data=data)
-
-plt.show()
-
+if __name__ == "__main__":
+    main()
